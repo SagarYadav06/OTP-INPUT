014b9bd76567b48f71df540e1a0f6b96b75606bd
sagarYadav <yadavfamilyinfo03@gmail.com>
2025-12-29 20:02:36 +0545

    initial project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Otp from './Components/otp'
 
 const App = () => {
   return (
-    <>
-    <Otp />
-    </>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
